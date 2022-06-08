740aff4eede42eb3ef0c03cce1e60fde709e00b3
liemnhatrang1999 <thanhliemnguyen2508@gmail.com>
2022-06-08 22:01:53 +0200

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn deployapi.wsgi
--- deployapi/settings.py
+++ deployapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4v4e_v^0hmc3ynrho0=p6tw$1zfhmt#b!7ygg_i-gc5sd94gsy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
